c7ccdef3c1e6511ca9984a27941ba929fdc61bae
SwpNimBased <110957807+SwpNimBased@users.noreply.github.com>
2022-08-10 12:54:14 +0800

    Create BasedGuiPrivate.lua

---
+++ Based/Hub/BasedGuiPrivate.lua
@@ -0,0 +1,33 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mapple7777/UI-Librarys/main/UI-1/UI.lua"))()
+local Window = Library:Create("Based Hub","You Are Using Private")
+
+--Main:
+local Tab1 = Window:Tab("Main",true)
+local Tab2 = Window:Tab("BedwarsHub",true)
+local Tab3 = Window:Tab("British Hub",true)
+local Tab4 = Window:Tab("ProHub",true)
+local Tab5 = Window:Tab("Update (Important)",true)
+
+--Useless:
+Tab1:Label("This Hub Made By KarlLavender")
+Tab1:Label("Special To British Hub,ProHub")
+Tab1:Label("Dont Worry Ill Add More Hubs")
+Tab1:Label("Thank U For Using Based Hub")
+--BedwarsHub:
+Tab2:Button("VapeV4|Must Have Vape File",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+end)
+Tab2:Button("Future",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/JNHHGaming/JN-HH-Gaming-Future-Is-Good/main/Future%20Fixed"))()
+end)
+Tab2:Button("JnHH Latest",function()
+    loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/1002510858917773352/1002895558790361158/untitled.txt"))()
+end)
+Tab2:Button("Soft Bed",function()
+    loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/947705523585835008/1005526028606775336/out.lua.txt",true))()
+end)
+--BritishHub:
+
+
+
+
