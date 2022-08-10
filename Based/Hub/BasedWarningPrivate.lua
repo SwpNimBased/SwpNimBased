05245a287bc0bf727ed3a904541355187adcce97
SwpNimBased <110957807+SwpNimBased@users.noreply.github.com>
2022-08-10 14:19:22 +0800

    Update BasedWarningPrivate.lua

--- Based/Hub/BasedWarningPrivate.lua
+++ Based/Hub/BasedWarningPrivate.lua
@@ -1,4 +1,4 @@
-local notifications = loadstring(game:HttpGet((""),true))()
+local notifications = loadstring(game:HttpGet(("https://raw.githubusercontent.com/SwpNimBased/SwpNimBased/main/Based/Hub/BasedKickPrivate"),true))()
 		notifications:notify{
 			Title = "Detected Player Not Listed",
 			Description = "U Are Being Kicked",
