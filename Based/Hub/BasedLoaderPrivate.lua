395f890056944ca4193a9918ad9d3b21673761ae
SwpNimBased <110957807+SwpNimBased@users.noreply.github.com>
2022-08-10 13:53:47 +0800

    Create BasedLoaderPrivate.lua

---
+++ Based/Hub/BasedLoaderPrivate.lua
@@ -0,0 +1,6 @@
+local notifications = loadstring(game:HttpGet(("https://raw.githubusercontent.com/SwpNimBased/SwpNimBased/main/Based/Hub/BasedGuiPrivate.lua"),true))()
+		notifications:notify{
+			Title = "Private Detected Whitelisted!",
+			Description = "WARNING THIS IS STILL BETA",
+			Accept = {
+				Text = "Accept"
